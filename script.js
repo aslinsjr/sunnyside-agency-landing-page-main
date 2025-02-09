e66301d4660f6c5b984b4be0538c88c2d4e0dc90
Alexandre Lins <aslinsjr@gmail.com>
2025-02-09 16:09:50 -0300

    Refatoração de código

--- script.js
+++ script.js
@@ -1,14 +1,51 @@
 
+const header = document.querySelector("header")
+const titleContainer = document.querySelector(".title-container")
+const innerFooter = document.querySelector(".inner-footer")
 
-document.addEventListener("wheel", () => {
-    document.querySelector("body").style.overflow = "auto"
-    document.querySelector(".title-container").opacity = "0"
-    document.querySelector("header").style.animation = "screenDown 1s"
-    document.querySelector("header").style.height = "10vh"
+window.scrollTo(0,0)
 
-    if (window.scrollY > 10 && window.scrollY < 90) {
-        document.querySelector("header").style.animation = "screenUp 1s"
-        document.querySelector("header").style.height = "100vh"
-        document.querySelector(".title-container").opacity = "1"
-    }
-})
+function scrollEffect() {
+    document.addEventListener("scroll", () => {
+
+        let height = document.body.scrollHeight
+
+        if( window.scrollY !== 0) {
+            header.classList.add("header-up")
+            titleContainer.style.display = "none"
+        } else {
+            titleContainer.style.display = "flex"
+            titleContainer.style.animation = "fadeIn 1s"
+            header.classList.remove("header-up")
+        }
+    
+        // if (window.scrollY > 0 && window.scrollY < 100) {
+        //     header.style.animation = "screenUp 1s"
+        //     header.style.height = "10vh"
+        //     
+            
+        // }
+    
+        // if  (window.scrollY === 0){
+        //     header.style.animation = "screenDown 1s"
+        //     header.style.height = "100vh"
+        //     
+        //     
+        // }
+        
+        if (window.scrollY === (height - innerHeight)) {
+            
+            header.style.opacity= "0" 
+            innerFooter.classList.add("footer-up")
+        } else {
+            header.style.opacity= "1" 
+            innerFooter.classList.remove("footer-up")
+            document.querySelector("footer").style.marginTop = "-30vh"
+        }
+    
+    })
+}
+
+scrollEffect()
+
+
