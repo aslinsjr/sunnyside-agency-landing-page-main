98ba1b8df7fd2b30678dd83257268a6acd4a5590
Alexandre Lins <aslinsjr@gmail.com>
2025-02-09 16:53:21 -0300

    Mobile Updates

--- script.js
+++ script.js
@@ -3,14 +3,13 @@ const header = document.querySelector("header")
 const titleContainer = document.querySelector(".title-container")
 const innerFooter = document.querySelector(".inner-footer")
 
-window.scrollTo(0,0)
 
 function scrollEffect() {
     document.addEventListener("scroll", () => {
 
         let height = document.body.scrollHeight
 
-        if( window.scrollY !== 0) {
+        if (window.scrollY !== 0) {
             header.classList.add("header-up")
             titleContainer.style.display = "none"
         } else {
@@ -18,34 +17,41 @@ function scrollEffect() {
             titleContainer.style.animation = "fadeIn 1s"
             header.classList.remove("header-up")
         }
-    
+
         // if (window.scrollY > 0 && window.scrollY < 100) {
         //     header.style.animation = "screenUp 1s"
         //     header.style.height = "10vh"
         //     
-            
+
         // }
-    
+
         // if  (window.scrollY === 0){
         //     header.style.animation = "screenDown 1s"
         //     header.style.height = "100vh"
         //     
         //     
         // }
-        
+
         if (window.scrollY === (height - innerHeight)) {
-            
-            header.style.opacity= "0" 
+
+            header.style.opacity = "0"
             innerFooter.classList.add("footer-up")
         } else {
-            header.style.opacity= "1" 
+            header.style.opacity = "1"
             innerFooter.classList.remove("footer-up")
             document.querySelector("footer").style.marginTop = "-30vh"
         }
-    
+
     })
 }
 
-scrollEffect()
+console.log()
+
+if (window.innerWidth > 500) {
+    scrollEffect()
+} else {
+    header.style.position = "unset"
+}
+
 
 
